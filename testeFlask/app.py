5baac41beec97f6c8c8dac0a44c8a9403dbc5742
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2023-06-07 09:37:11 -0300

    add db

--- testeFlask/app.py
+++ testeFlask/app.py
@@ -1,11 +1,32 @@
 from flask import *
+from Controle.classConexao import Conexao
 import os
 
+
 app = Flask(__name__)
+conexaoBanco = Conexao(os.getenv("DBNAME"),os.getenv("HOST"),os.getenv("PORT"),os.getenv("USER"),os.getenv("PASSWORD"))
 
 @app.route("/")
 def index():
-    return f"bem vindo {os.getenv('teste')}"
+    return "bem vindo"
+
+@app.route("/version")
+def version():
+
+    return conexaoBanco.consultarBanco("Select version()")
+
+@app.route("/Pokemons")
+def listaPokemon():
+
+    pokedex = conexaoBanco.consultarBanco('''Select * From "Pokedex"''')
+
+    listaPokemons = []
+
+    for pokemon in pokedex:
+
+        listaPokemons.append({'id':pokemon[0], 'especie':pokemon[1]})
+
+    return listaPokemons
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
